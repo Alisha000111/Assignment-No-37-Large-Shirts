83097771bd39fa24b686ae03efd18fdf0d7d293e
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-10 01:49:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function makeShirt(size, text) {
+    if (size === void 0) { size = 'large'; }
+    if (text === void 0) { text = 'I love typescript'; }
+    console.log("You have order a ".concat(size, ", shirt that says ").concat(text));
+}
+makeShirt();
+makeShirt("medium");
+// different message
+makeShirt('small', 'I need a big shirt to wear');
---
+++ app.ts
@@ -0,0 +1,8 @@
+// Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function makeShirt(size: string = 'large', text: string = 'I love typescript'): void {
+    console.log(`You have order a ${size}, shirt that says ${text}`)
+}
+makeShirt();
+makeShirt(`medium`)
+// different message
+makeShirt('small', 'I need a big shirt to wear')
